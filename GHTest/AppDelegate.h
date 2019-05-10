d02b95537b3400326ea707467f743ff112a67159
liliyasayfutdinova <liliyasayfutdinova@netlabsystems.com>
2019-05-10 20:20:16 +0500

    Initial Commit

---
+++ GHTest/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  GHTest
+//
+//  Created by Liliya Sayfutdinova on 10/05/2019.
+//  Copyright © 2019 Liliya Sayfutdinova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ GHTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GHTest
+//
+//  Created by Liliya Sayfutdinova on 10/05/2019.
+//  Copyright © 2019 Liliya Sayfutdinova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
